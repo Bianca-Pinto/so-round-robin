9b46fd62c231bea8d6160cff5d0f8c09b972ae5e
bianca-pinto <71729898+Bianca-Pinto@users.noreply.github.com>
2023-04-16 20:09:44 -0300

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,69 +1,74 @@
 #include<stdio.h>
+#define TAM 10
  
-int main()
-{
-      int i, limit, total = 0, x, counter = 0, time_quantum;
-      int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10];
-      float average_wait_time, average_turnaround_time;
-      printf("\nEnter Total Number of Processes:\t");
-      scanf("%d", &limit);
-      x = limit;
-      for(i = 0; i < limit; i++)
-      {
-            printf("\nEnter Details of Process[%d]\n", i + 1);
+int main(){
+      int i, nProcessos, somaTempo = 0, x, contador = 0, quantum;
+      // TAM --> tamanho do vetor, declarado para ingresso, duracao e temp
+      // Deve ser pelo menos o tamanho ou maior do que o numero de tarefas
+      int tempo_espera = 0, tempo_execucao = 0, ingresso[TAM], duracao[TAM], temp[TAM];
+      float tempo_medio_espera, tempo_medio_execucao;
+
+      // Inserindo o numero total de tarefas
+      printf("\nInsira numero total de Processos:\t");
+      scanf("%d", &nProcessos);
+      x = nProcessos;
+
+      for(i = 0; i < nProcessos; i++){
+            printf("\nInsira detalhes da tarefa [t%d]\n", i + 1);
+            // Inserindo o tempo de ingresso de cada tarefa
+            printf("Ingresso:\t");
+            scanf("%d", &ingresso[i]);
  
-            printf("Arrival Time:\t");
+            // Inserindo o tempo de duracao de cada tarefa
+            printf("Duracao:\t");
+            scanf("%d", &duracao[i]);
  
-            scanf("%d", &arrival_time[i]);
- 
-            printf("Burst Time:\t");
- 
-            scanf("%d", &burst_time[i]);
- 
-            temp[i] = burst_time[i];
+            // Criando um vetor temporario para cada duracao
+            temp[i] = duracao[i];
       }
  
-      printf("\nEnter Time Quantum:\t");
-      scanf("%d", &time_quantum);
-      printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\n");
-      for(total = 0, i = 0; x != 0;)
-      {
-            if(temp[i] <= time_quantum && temp[i] > 0)
-            {
-                  total = total + temp[i];
+      // Definicao do quantum (tempo de ciclo para o escalonamento)
+      printf("\nInsira o Quantum:\t");
+      scanf("%d", &quantum);
+
+      printf("\nProcesso n\t\t Duracao\t Execucao\t Espera\n");
+      for(somaTempo= 0, i = 0; x!=0;){
+            // Se a duracao for menor ou igual ao quantum e maior do que 0
+            if(temp[i] <= quantum && temp[i] > 0){
+                  somaTempo = somaTempo + temp[i];
                   temp[i] = 0;
-                  counter = 1;
+                  contador = 1;
             }
-            else if(temp[i] > 0)
-            {
-                  temp[i] = temp[i] - time_quantum;
-                  total = total + time_quantum;
+            // Se a duracao da tarefa for maior que o quantum
+            else if(temp[i] > 0){
+                  temp[i] = temp[i] - quantum;
+                  somaTempo = somaTempo + quantum;
             }
-            if(temp[i] == 0 && counter == 1)
-            {
-                  x--;
-                  printf("\nProcess[%d]\t\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
-                  wait_time = wait_time + total - arrival_time[i] - burst_time[i];
-                  turnaround_time = turnaround_time + total - arrival_time[i];
-                  counter = 0;
+            // Confere se a duracao do processo chegou ao fim
+            if(temp[i] == 0 && contador == 1){
+                  x--; //decrementa o numero do processo
+                  printf("\nProcesso[%d] \t\t %d\t\t %d\t\t\t %d", i + 1, duracao[i], somaTempo - ingresso[i], somaTempo - ingresso[i] - duracao[i]);
+                  tempo_espera = tempo_espera + somaTempo - ingresso[i] - duracao[i];
+                  tempo_execucao = tempo_execucao + somaTempo - ingresso[i];
+                  contador = 0;
             }
-            if(i == limit - 1)
-            {
+            // Confere se i chegou ao final da lista de processos
+            if(i == nProcessos-1){
                   i = 0;
             }
-            else if(arrival_time[i + 1] <= total)
-            {
+            // Verifica se algum processo ingressou enquanto o outro estava executando
+            else if(ingresso[i+1] <= somaTempo){
                   i++;
             }
-            else
-            {
+            else{
                   i = 0;
             }
       }
  
-      average_wait_time = wait_time * 1.0 / limit;
-      average_turnaround_time = turnaround_time * 1.0 / limit;
-      printf("\n\nAverage Waiting Time:\t%f", average_wait_time);
-      printf("\nAvg Turnaround Time:\t%f\n", average_turnaround_time);
+      tempo_medio_execucao = (float)tempo_execucao/nProcessos;
+      printf("\nTempo medio de execucao:\t%f\n", tempo_medio_execucao);
+
+      tempo_medio_espera = (float)tempo_espera/nProcessos;
+      printf("\n\nTempo medio de espera:\t%f", tempo_medio_espera);
       return 0;
 }
